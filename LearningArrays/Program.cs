47e963d9932e43707c1b374be9671406a941e942
Sandra <sandra.aleksiunaite@gmail.com>
2021-11-08 12:48:02 +0200

    Code commitement

---
+++ LearningArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LearningArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] Colors = { "Balta", "Zalia", "Geltona", "Raudona", "Melyna" };
+
+            Console.WriteLine("Array of colors:");
+
+            foreach (String a in Colors)
+
+                Console.WriteLine(a);
+            
+            int[] Numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+
+            Console.WriteLine("\nArray of numbers:");
+
+            for (int i = Numbers.Length -1; i >=0 ; i--)
+            
+                Console.WriteLine(Numbers[i]);
+            
+        }
+    }
+}
